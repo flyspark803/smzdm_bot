184a0a6f699de5944d28fa64b2d3a407c3f960aa
flyspark803 <65171060+flyspark803@users.noreply.github.com>
2024-05-16 11:49:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class SMZDM_Bot(object):
         cookie 为浏览器复制来的字符串
         :param cookie: 登录过的社区网站 cookie
         """
-        self.session.headers['Cookie'] = cookies    
+        self.session.headers['Cookie'] = __ckguid=CRc2wjbabCoSEw82QPiPfXf7; device_id=21307064331712103563594907f0b1287c9de56591419711ae04995e4b; homepage_sug=a; r_sort_type=score; sess=BA-1U%2BeA%2BV8jHvo%2FGkdWvw5BrcMXiGdKF%2B9ozkw4ytystZVqFTwlNF%2BZn273ctFJBRMYu1OK65%2FL5Wo%2BnaWzM1oO0VXP%2BuGR%2FXsuvd0LvHuVbvLt4wFWcAiIhYY; user=user%3A5404570445%7C5404570445; smzdm_id=5404570445; _zdmA.vid=*; footer_floating_layer=0; ad_date=16; ad_json_feed=%7B%7D; _zdmA.uid=ZDMA.OE3VZzBb1.1715831081.2419200; bannerCounter=%5B%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A8%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A3%2C%22surplus%22%3A1%7D%5D; _zdmA.time=1715831230048.0.https%3A%2F%2Fwww.smzdm.com%2F    
 
     def checkin(self):
         """
